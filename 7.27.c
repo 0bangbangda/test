910a03eb46147160cb96095ca40f66c78161ca35
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-07-27 17:34:56 +0800

    Add files via upload

---
+++ 7.27.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+//void menu()
+//{
+//	printf("########################\n");
+//	printf("######1.play  0.exit#####\n");
+//	printf("########################\n");
+//}
+//void game()
+//{
+//	int geuss = 0;
+//	int ret = 0;
+//	srand((unsigned)time(NULL));
+//	ret = rand() % 100 + 1;
+//	printf("请猜数字\n");
+//	while (1)
+//	{
+//		scanf("%d", &geuss);
+//		if (geuss == ret)
+//		{
+//			printf("猜对了\n");
+//			break;
+//		}
+//		else if (geuss > ret)
+//			printf("猜大了\n");
+//		else if (geuss < ret)
+//			printf("猜小了\n");
+//	}
+//}
+//int main()
+//{
+//	int input = 0;
+//	do
+//	{
+//		printf("请选择>:\n");
+//		menu();
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//		{
+//				  game();
+//				  break;
+//		}
+//		case 0:
+//		{
+//				  printf("退出程序\n");
+//				  break;
+//		}
+//			
+//		default:
+//			printf("输入错误");
+//		}
+//	} while (input);
+//	return 0;
+//}
+#include<math.h>
+//void Judge(int x)
+//{
+//	int i = 0;
+//	for (i = 2; i <= sqrt((float)x); i++)
+//	{
+//		if (0 == x%i)
+//		{
+//			printf("不是素数\n");
+//			break;
+//		}
+//
+//	}
+//	if (i > sqrt((float)x))
+//		printf("是素数\n");
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	Judge(a);
+//	return 0;
+//}
+//void Judge(int x)
+//{
+//	if ((0 == x % 4) && (0 != x % 100) || (0 == x % 400))
+//		printf("是闰年\n");
+//	else
+//		printf("不是闰年\n");
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	Judge(a);
+//	return 0;
+//}
