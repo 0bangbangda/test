0543e8ea2121ed42aee354f6620be14afe92d93e
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-08-05 21:31:13 +0800

    Add files via upload

---
+++ 8.5.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a = -1;
+//	int b = 1;
+//	printf("%d\n", a&b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int count = 0;
+//	int i = 0;
+//	scanf("%d", &a);
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((a >> i) & 1 == 1)
+//			count++;
+//	}
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+//void test1(int arr[])
+//{
+//	printf("%d\n", sizeof(arr));
+//}
+//void test2(char arr[])
+//{
+//	printf("%d\n", sizeof(arr));
+//}
+//int main()
+//{
+//	int arr1[10] = { 0 };
+//	char arr2[10] = { 0 };
+//	test1(arr1);
+//	test2(arr2);
+//}
+int main()
+{
+	/*int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = ++b&&++d&&a++;
+	printf("a=%d\nb=%d\nc=%d\nd=%d\n", a, b, c, d);
+	return 0;*/
+	int a = 10;
+	int b = 20;
+	/*a > b ? printf("a>b\n") : printf("a<b\n");*/
+
+}
