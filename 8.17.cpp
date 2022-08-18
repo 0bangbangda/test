40994df40e061226a31759dd8864b02e644d131a
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-08-18 10:30:57 +0800

    Add files via upload

---
+++ 8.17.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<assert.h>
+char* my_strcpy(char* dest, const char* src)
+{
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[] = "#########";
+	char arr2[] = "bit";
+	printf("%s\n", my_strcpy(arr1, arr2));
+	return 0;
+}
