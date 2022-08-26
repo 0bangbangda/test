2c4310c51a2d6105d158ca5a58165c5da57a4deb
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-08-26 17:28:04 +0800

    Add files via upload

--- 8.26.cpp
+++ 8.26.cpp
@@ -66,3 +66,62 @@
 //	}
 //	return 0;
 //}
+//汽水的属性
+//单价  换一瓶汽水所需的空瓶数
+//struct water
+//{
+//	int price;
+//	int blank;
+//};
+//int main()
+//{
+//	//喝汽水问题
+//	struct water s = { 1, 2 };
+//	int money = 0; 
+//	scanf("%d", &money);
+//	int num = money/(s.price);
+//	int empty = num;
+//	while (empty / 2)
+//	{
+//		num += empty / (s.blank);
+//		empty = empty / (s.blank) + empty % (s.blank);
+//	}
+//	printf("%d\n", num);
+//	return 0;
+//
+//}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//趟数
+	int i = 0;
+	for (i = 0; i < sz/2+1; i++)
+	{
+		//判断奇偶
+		int j = 0; 
+		int flag = 1;
+		for (j = 0; j < sz - 1; j++)
+		{
+			
+			if (arr[j] % 2 == 0 && arr[j + 1] % 2 == 1)
+				//交换位置
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+				flag = 0;
+			}
+			
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
