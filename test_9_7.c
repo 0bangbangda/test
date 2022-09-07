c91a98e98b3c9142d48d443f4c0a11d15f98288e
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-09-07 21:53:06 +0800

    Add files via upload

---
+++ test_9_7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* int Max(int x,int y); */
+/* int main(void){ */
+/*   int a,b; */
+/*   scanf("%d%d",&a,&b); */
+/*   int max=Max(a,b); */
+/*   printf("max = %d\n",max); */
+/*   return EXIT_SUCCESS; */
+/* } */
+/* int Max(int x,int y){ */
+/*   int z; */
+/*   z=x>y?x:y; */
+/*   return z; */
+/* } */
+
