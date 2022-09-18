4cb28f48668d73315c53444fd8023e9959272a9f
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-09-18 11:28:37 +0800

    Add files via upload

---
+++ test_9_18.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* void Calc(double a,int b,int c,double* r1,double* r2); */
+/* int main(void) */
+/* { */
+/*   double a;int b;int c;double r1,r2; */
+/*   /\*a=e.10^(-13),b=-200,c=1*\/ */
+/*   scanf("%lf%d%d",&a,&b,&c); */
+/*   Calc(a,b,c,&r1,&r2); */
+/*   printf("r1 = %e, r2 = %e\n",r1,r2);/\*r1=-1.990000e-09,r2=2.010000e-09*\/ */
+/*   return EXIT_SUCCESS; */
+  
+/* } */
+/* void Calc(double a,int b,int c,double* r1,double *r2) */
+/* { */
+/*   double delta=b*b-4*a*c; */
+/*   *r1=(-b-delta)/2*a; */
+/*   *r2=(-b+delta)/2*a; */
+/* } */
+/* int main(void) */
+/* { */
+/*   int C;double F; */
+/*   /\*C=3*\/ */
+/*   scanf("%d",&C); */
+/*   F=9.0*C/5+32; */
+/*   printf("F = %f\n",F);/\*F=37.400000*\/ */
+/*   return EXIT_SUCCESS; */
+/* } */
+int main(void)
+{
+  enum couleur {ROUGE,VERT,BLEU,JAUNE};
+  return EXIT_SUCCESS;
+}
