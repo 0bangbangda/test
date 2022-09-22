27147447ace7e6da8f8abfda5c1de8a87dcd8157
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-09-22 09:47:28 +0800

    Add files via upload

---
+++ test_9_22.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* int factorielleC(int n); */
+/* long factorielleD(int n); */
+/* int main(void){ */
+/*   int n; */
+/*   scanf("%d",&n); */
+/*   /\*comparasion entre factorielleC et factorielleD *\/ */
+/*   printf("%d! = %d\n",n,factorielleC(n)); */
+/*   printf("%d! = %ld\n",n,factorielleD(n)); */
+/*   /\*la plus grande valeur de n est 12*\/ */
+/*   return EXIT_SUCCESS; */
+/* } */
+/* int factorielleC(int n) */
+/* { */
+/*   int fact=1,i=1; */
+/*   while(i<=n){ */
+/*     fact=fact*i; */
+/*     i=i+1; */
+/*   } */
+/*   return fact; */
+
+/* } */
+/* long factorielleD(int n) */
+/* { */
+/*   long fact=1,i=(long)n; */
+/*   while(i>0){ */
+/*     fact*=i; */
+/*     i--; */
+/*   } */
+/*   return fact; */
+
+/* } */
+/* int division(const int a,const int b); */
+/* int main(void) */
+/* { */
+/*   int a,b; */
+/*   scanf("%d %d",&a,&b); */
+/*   printf("q = %d\n",division(a,b)); */
+/*   return EXIT_SUCCESS; */
+/* } */
+/* int division(const int a,const int b) */
+/* { */
+/*   int q=0,r=a; */
+/*   while(r>b){ */
+/*     r=r-b; */
+/*     q++; */
+/*   } */
+/*   return q; */
+/* } */
+int produit(const int x, int y);
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("p = %d\n", produit(a, b));
+	return EXIT_SUCCESS;
+}
+int produit(const int x, int y)
+{
+	int p = 0;
+	while (y>0){
+		y = y - 1;
+		p = p + x;
+	}
+	return p;
+
+}
