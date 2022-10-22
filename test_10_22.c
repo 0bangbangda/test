1693443998e3295870731534e3ca3dcadfe31122
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-10-22 17:06:05 +0800

    Add files via upload

--- test_10_22.c
+++ test_10_22.c
@@ -1,19 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+//int main(void){
+//	int arr[9];
+//	for (int i = 0; i < 9; i++){
+//		scanf("%d", &arr[i]);
+//	}
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 0; i < 9; i++){
+//		if (n>arr[i] && n < arr[i+1] && i+1 < 9){
+//			printf("%d\n", arr[i]);
+//			printf("%d\n", n);
+//		}
+//		else
+//			printf("%d\n", arr[i]);
+//	}
+//	return EXIT_SUCCESS;
+//}
+//输入10个数字，然后逆序输出。
+//逆序输出，空格分开
 int main(void){
-	int arr[9];
-	for (int i = 0; i < 9; i++){
+	int arr[10];
+	for (int i = 9; i >= 0; i--){
 		scanf("%d", &arr[i]);
 	}
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < 9; i++){
-		if (n>arr[i] && n < arr[i+1] && i+1 < 9){
-			printf("%d\n", arr[i]);
-			printf("%d\n", n);
-		}
-		else
-			printf("%d\n", arr[i]);
+	for (int i = 0; i < 10; i++){
+		printf("%d ", arr[i]);
 	}
 	return EXIT_SUCCESS;
 }
