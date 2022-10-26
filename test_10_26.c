6de6233e2c5d9d453e630a733a564adb5dab7e50
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-10-26 21:52:12 +0800

    Add files via upload

--- test_10_26.c
+++ test_10_26.c
@@ -3,30 +3,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
-void count(char arr[], int*p1, int*p2, int*p3, int*p4);
+//void count(char arr[], int*p1, int*p2, int*p3, int*p4);
+//int main(void){
+//	char arr[50];
+//	gets_s(arr, 40);
+//	int num1, num2, num3, num4;
+//	count(arr, &num1, &num2, &num3, &num4);
+//	printf("%d %d %d %d", num1, num2, num3, num4);
+//	return EXIT_SUCCESS;
+//}
+//void count(char arr[], int*p1, int*p2, int*p3, int*p4)
+//{
+//	int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+//	int i = 0;
+//	while (arr[i] != '\0')
+//	{
+//		if (isalpha(arr[i]))
+//			num1++;
+//		else if (isdigit(arr[i]))
+//			num2++;
+//		else if (isblank(arr[i]))
+//			num3++;
+//		else
+//			num4++;
+//		i++;
+//	}
+//	*p1 = num1; *p2 = num2; *p3 = num3; *p4 = num4;
+//}
+//定义一个带参的宏，使两个参数的值互换，并写出程序，输入两个数作为使用宏时的实参。输出已交换后的两个值。
+
+#define exchange(x,y) tmp=x,x=y,y=tmp;
 int main(void){
-	char arr[50];
-	gets_s(arr, 40);
-	int num1, num2, num3, num4;
-	count(arr, &num1, &num2, &num3, &num4);
-	printf("%d %d %d %d", num1, num2, num3, num4);
+	int a, b;
+	int tmp;
+	scanf("%d %d", &a, &b);
+	exchange(a, b);
+	printf("%d %d", a, b);
 	return EXIT_SUCCESS;
-}
-void count(char arr[], int*p1, int*p2, int*p3, int*p4)
-{
-	int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
-	int i = 0;
-	while (arr[i] != '\0')
-	{
-		if (isalpha(arr[i]))
-			num1++;
-		else if (isdigit(arr[i]))
-			num2++;
-		else if (isblank(arr[i]))
-			num3++;
-		else
-			num4++;
-		i++;
-	}
-	*p1 = num1; *p2 = num2; *p3 = num3; *p4 = num4;
 }
