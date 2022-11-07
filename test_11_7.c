ce1d27d91ba37ce230c952101c127363604060ce
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-11-07 12:00:42 +0800

    Add files via upload

--- test_11_7.c
+++ test_11_7.c
@@ -21,3 +21,19 @@
 //	printf("%s", arr);
 //	return EXIT_SUCCESS;
 //}
+#include<assert.h>
+void time(int t, int*h, int*m, int*s);
+int main(void){
+	int t, h, m, s;
+	scanf("%d", &t);
+	assert(t >= 0 && t <= 86399);
+	time(t, &h, &m, &s);
+	printf("%d:%d:%d", h, m, s);
+	return EXIT_SUCCESS;
+}
+void time(int t, int*h, int*m, int*s)
+{
+	*h = t / 3600;
+	*m = t % 3600 / 60;
+	*s = t % 3600 % 60;
+}
