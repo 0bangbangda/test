afeabb98c9d432d44518cb5322e77b54bbdef712
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-11-07 09:58:45 +0800

    Add files via upload

---
+++ test_11_7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//int main(void){
+//	int x = 1, y = 2;
+//	if (x > y&&x != y);
+//	return EXIT_SUCCESS;
+//}
+//int main(void){
+//	/*char c = ' ';
+//	printf("%d",c);*/
+//	char arr[101] = { 0 };
+//	scanf("%[^\n]", arr);
+//	int left = 0, right = strlen(arr) - 1;
+//	while (left < right){
+//		char tmp = arr[right];
+//		arr[right] = arr[left];
+//		arr[left] = tmp;
+//		left++; right--;
+//	}
+//	printf("%s", arr);
+//	return EXIT_SUCCESS;
+//}
