7d36d93e1b3d680d00c54d212b73eeaac8ffe16a
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-12-01 23:00:06 +0800

    Add files via upload

---
+++ test_12_1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main(void){
+//	/*int n = 0;
+//	while (n++ <= 2);
+//	printf("%d", n);*/
+//	char c;
+//	while ((c = getchar()) != '\n'){
+//		if (c >= 'a'&&c <= 'z')
+//			c=
+//	}
+//	return EXIT_SUCCESS;
+//}
+//#include<string.h>
+//#include<errno.h>
+//int main(void){
+//	/*int a = 106;
+//	printf("%d\n", a);
+//	printf("%d\n", printf("%d\n", a));
+//	printf("%d\n", printf("%d\n", printf("%d\n", a)));*/
+//	//int a, b;
+//	/*scanf("%c,", &a, &b);*/
+//	/*printf("%d %d\n", a, b);*/
+//	int *p =malloc(INT_MAX);
+//	printf("%s", strerror(errno));
+//	return EXIT_SUCCESS;
+//}
