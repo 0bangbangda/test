f3a6fb0cccc153cd0f2ba2c6f0d1afdd778d3c74
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2022-12-07 19:28:58 +0800

    Add files via upload

--- test_12_5.c
+++ test_12_5.c
@@ -2,33 +2,52 @@
 #include<stdlib.h>
 #define LG_MAX 100
 typedef int BIGNATS[LG_MAX];
+enum op{Add=1,Sous,Multi,Div,Modul,Cmp};
 void zero(BIGNATS b);
 void print(BIGNATS b);
 void caractere(BIGNATS b);
 void trans(BIGNATS b,int n);
 void lire(BIGNATS b);
+void lecture(BIGNATS b1, BIGNATS b2);
+void liste();
 int main(void){
-	BIGNATS b;
+	//BIGNATS b;
 	/*zero(b);
 	caractere(b);
 	print(b);*/
-	BIGNATS b1, b2;
-	printf("b1:");
-	lire(b1);
-	printf("b2:");
-	lire(b2);
+	BIGNATS b1, b2, b3;
+	zero(b1);
+	zero(b2);
+	zero(b3);
+	/*print(b1);
+	printf("\n");
+	print(b2);*/
 	char op;
 	scanf("%c", &op);
 	switch (op){
-	case '+':                                
-		Add();
+	case '+':    
+		lecture(b1, b2);
+		Add(b1,b2,b3);
 		break;   
 	case '-':
+		lecture(b1, b2);
+		Sous(b1, b2, b3);
 		break;
 	case '*':
+		lecture(b1, b2);
+		Multi(b1, b2, b3);
 		break;
 	case '/':
+		lecture(b1, b2);
+		Div(b1, b2, b3);
 		break;
+	case '%':
+		lecture(b1, b2);
+		Modul(b1, b2, b3);
+		break;
+	case 'c':
+		int ret = Cmp(b1, b2);
+		printResult(ret);
 	}
 	return EXIT_SUCCESS;
 }
@@ -40,9 +59,14 @@ void zero(BIGNATS b)
 }
 void print(BIGNATS b)
 {
-	for (int i = 0; i < LG_MAX; i++){
-		printf("%d ", b[i]);
-	}
+	int m;
+	for (m = 0; m < LG_MAX; m++)
+	if (!b[m])
+		break;
+	for (int i = m - 1; i >= 0; i--)
+		printf("%d", b[i]);
+	printf("\n");
+
 }
 void caractere(BIGNATS b)
 {
@@ -65,5 +89,25 @@ void trans(BIGNATS b,int n)
 	b[indice] = n;
 }
 void lire(BIGNATS b){
+	char arr[LG_MAX] = { 0 };
+	scanf("%s", arr);
+	int m;
+	for (m = 0; m < LG_MAX; m++)
+	if (!arr[m])
+		break;
+	for (int i = m-1, j = 0; i >= 0; i--, j++){
+		b[j] = arr[i] - '0';
+	}
+}
+void lecture(BIGNATS b1, BIGNATS b2)
+{
+	printf("b1:");
+	lire(b1);
+	printf("b2:");
+	lire(b2);
+}
+void liste()
+{
+	printf("choisir:\n");
 	
 }
