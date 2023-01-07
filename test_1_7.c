5d73e0f00a8f33ebc74f6e49126f7ed8691b007f
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2023-01-07 11:34:29 +0800

    Add files via upload

---
+++ test_1_7.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#define Max 10
+/* int g_val=100; */
+/* int Add(int x,int y); */
+/* int main(void){ */
+/*   int arr[Max];//hehe */
+/*   for(int i=0;i<Max;i++)//12345 */
+/*     arr[i]=i; */
+/*   return EXIT_SUCCESS; */
+/* } */
+/* int Add(int x,int y) */
+/* { */
+/*   return x+y; */
+/* } */
+#include<windows.h>
+int main(void)
+{
+	//Ô¤¶¨Òå·ûºÅ
+	/*printf("%s\n", __FILE__);
+	printf("%d\n", __LINE__);
+	printf("%s\n", __DATE__);
+	printf("%s\n", __TIME__);*/
+	while (1){
+		printf("%s\n", __TIME__);
+		Sleep(1000);
+	}
+	return EXIT_SUCCESS;
+}
