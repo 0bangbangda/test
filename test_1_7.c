e09a272056cb49daddacf72149cf020d1876ca8f
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2023-01-07 17:45:54 +0800

    Add files via upload

--- test_1_7.c
+++ test_1_7.c
@@ -13,17 +13,33 @@
 /* { */
 /*   return x+y; */
 /* } */
-#include<windows.h>
-int main(void)
-{
-	//预定义符号
-	/*printf("%s\n", __FILE__);
-	printf("%d\n", __LINE__);
-	printf("%s\n", __DATE__);
-	printf("%s\n", __TIME__);*/
+//#include<windows.h>
+//int main(void)
+//{
+//	//预定义符号
+//	/*printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%s\n", __DATE__);
+//	printf("%s\n", __TIME__);*/
+//	while (1){
+//		printf("%s\n", __TIME__);
+//		Sleep(1000);
+//	}
+//	return EXIT_SUCCESS;
+//}
+int main(void){
+	int arr1[10];
+	char arr2[] = { 'a', 'b', 'c', 'd', 'f', 'e' };
+	int i = 0, j = 0;
+	for (i = 0; i < 10; i++)
+		arr1[i] = i;
+	for (i = 0; i < 10; i++)
+		printf("arr1[%d] address:%#p\n", i, &arr1[i]);
 	while (1){
-		printf("%s\n", __TIME__);
-		Sleep(1000);
+		printf("arr2[%d] address:%#p\n", j, &arr2[j]);
+		if (arr2[j] == 'e')
+			break;
+		j++;
 	}
 	return EXIT_SUCCESS;
 }
