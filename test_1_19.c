2e5d61c77cdd1145507b1556568eaf0cd201cf76
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2023-01-19 17:25:56 +0800

    Add files via upload

--- test_1_19.c
+++ test_1_19.c
@@ -9,14 +9,25 @@
 //	return EXIT_SUCCESS;
 //}
 //用二维数组指针遍历整个数组
+//int main(void){
+//	int arr[3][4] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+//	int(*p)[4] = arr;
+//	for (int i = 0; i < 3; i++){
+//		for (int j = 0; j < 4; j++){
+//			printf("%2d", *(*(p + i) + j));
+//		}
+//		putchar('\n');
+//	}
+//	return EXIT_SUCCESS;
+//}
+//函数指针
+int Max(int a, int b)
+{
+	return (a > b ? a : b);
+}
 int main(void){
-	int arr[3][4] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-	int(*p)[4] = arr;
-	for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 4; j++){
-			printf("%2d", *(*(p + i) + j));
-		}
-		putchar('\n');
-	}
+	int x = 10, y = 20;
+	int(*pMax)(int a, int b) = Max;
+	printf("%d\n", (*pMax)(x, y));
 	return EXIT_SUCCESS;
 }
