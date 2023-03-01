c51b19a1df0fb54532d5ac24b48b0c98bca7c1fa
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2023-03-01 16:36:46 +0800

    Create test_3_1.c

---
+++ test_3_1.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    if(head==NULL){
+        return head;
+    }
+    struct ListNode *dummy=(struct ListNode*)malloc(sizeof(struct ListNode));
+    dummy->next=head;
+    struct ListNode *cur=dummy;
+    while(cur->next&&cur->next->next){
+        if(cur->next->val==cur->next->next->val){
+            int x=cur->next->val;
+            while(cur->next&&cur->next->val==x){
+                cur->next=cur->next->next;
+            }
+        }
+        else{
+            cur=cur->next;
+        }
+        }
+        return dummy->next;
+    }
