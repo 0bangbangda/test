371e86c66cc12f63adb231557e3ef9b454cc0dd8
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2023-03-08 10:59:53 +0800

    Add files via upload

---
+++ test_3_8.c
@@ -0,0 +1,11 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main(void){
+//	int a;
+//	scanf("%d", &a);
+//	int b;
+//	scanf("%d", &b);
+//	int c = a + b;
+//	printf("%d\n", c);
+//	return EXIT_SUCCESS;
+//}
