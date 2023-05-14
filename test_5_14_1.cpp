4c253ac7de4a88f0f4126280162625a2b966eaea
0bangbangda <109780922+0bangbangda@users.noreply.github.com>
2023-05-14 21:02:33 +0800

    Add files via upload

---
+++ test_5_14_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int a, b;
+	cin >> a >> b;
+	bool flag = a > b;
+	flag = true;
+	cout << flag << endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
